677725af287232bc37307f5dfa67ceb1dca81b34
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2021-10-15 21:34:42 -0500

    se cambia host

--- gvi/settings.py
+++ gvi/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-^4j9lisr66)&f#q6f#q_$bbu@pvvm+^5np&mz5%!%tpik^lo@c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.14.1.165','127.0.0.1']
+ALLOWED_HOSTS = ['18.219.75.111','127.0.0.1']
 
 
 # Application definition
