21c78db632082748fe1ffdde640d1ab82af762f3
Diego Osorio <juan.osorio1599@alumnos.udg.mx>
2021-10-15 23:50:07 -0500

    Funcion POST recibe datos

--- api/views.py
+++ api/views.py
@@ -6,6 +6,8 @@ from django.shortcuts import render
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import JsonResponse
+from apps.test import test
+import json
 
 # Create your views here.
 # def apiView(request):
@@ -19,5 +21,8 @@ class ApiView(View):
         return super().dispatch(request, *args, **kwargs)
     
     def post(self, request, *args, **kwargs):
-        datos = {'message': 'Success'}
+        #jd = json.load(request.body)
+        print(request.body)
+        test()
+        datos = {'holi' : "siiiuuu"}
         return JsonResponse(datos)
---
+++ apps/test.py
@@ -0,0 +1,3 @@
+def test():
+    for i in range(10):
+        print(i)
